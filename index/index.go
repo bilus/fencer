2e90c332fc72097f8a5057334e9628dd4cf1dd7a
Marcin Bilski <gyamtso@gmail.com>
2018-02-09 17:39:44 +0100

    Fix naming of the argument controlling bounds around a point if Find query.

--- index/index.go
+++ index/index.go
@@ -20,13 +20,13 @@ func New(features []feature.Feature) (*Index, error) {
 	return &index, nil
 }
 
-// Find returns a slice of features intersecting a square of sideMeters around a given point.
-// - point, sideMeters - the range of a spatial query
+// Find returns a slice of features intersecting a square around a given `point` covering the `distance` radius.
+// - point, distance - the range of a spatial query
 // - preconditions - a conjunction of predicates, set to nil to pass all features matching the spatial query
 // - filters - a disjunction of predicates filtering the result from predicates + definition of distinctness, set to nil to pass all features
 // - reducer - defines how to combine results together (a reduce function), set to nil to simply store it under one or more keys returned by the filters
-func (index *Index) Find(point primitives.Point, sideMeters float64, preconditions []query.Condition, filters []query.Filter, reducer query.Reducer) ([]feature.Feature, error) {
-	bounds, err := geomBoundsAround(point, sideMeters)
+func (index *Index) Find(point primitives.Point, distance float64, preconditions []query.Condition, filters []query.Filter, reducer query.Reducer) ([]feature.Feature, error) {
+	bounds, err := geomBoundsAround(point, distance)
 	if err != nil {
 		return nil, err
 	}
@@ -45,8 +45,8 @@ func (index *Index) Find(point primitives.Point, sideMeters float64, preconditio
 	return query.MatchingFeatures(), nil
 }
 
-func geomBoundsAround(point primitives.Point, sideMeters float64) (*rtreego.Rect, error) {
-	bound := geo.NewGeoBoundAroundPoint(geo.NewPoint(point[0], point[1]), sideMeters)
+func geomBoundsAround(point primitives.Point, distance float64) (*rtreego.Rect, error) {
+	bound := geo.NewGeoBoundAroundPoint(geo.NewPoint(point[0], point[1]), distance)
 	tl := bound.SouthWest()
 	return rtreego.NewRect(rtreego.Point{tl[0], tl[1]}, []float64{bound.Width(), bound.Height()})
 }
