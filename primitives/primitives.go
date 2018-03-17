138953d17c636d7bf02a3a97b69c437a6bd6ac92
Marcin Bilski <gyamtso@gmail.com>
2018-03-17 11:37:58 +0100

    Hide rtreego implementation.

--- primitives/primitives.go
+++ primitives/primitives.go
@@ -4,4 +4,18 @@ import (
 	"github.com/bilus/rtreego"
 )
 
+type Rect rtreego.Rect
+
+func NewRect(p Point, lengths ...float64) (*Rect, error) {
+	r, err := rtreego.NewRect(rtreego.Point(p), lengths)
+	if err != nil {
+		return nil, err
+	}
+	return (*Rect)(r), nil
+}
+
 type Point rtreego.Point
+
+func (p Point) MinDist(r *Rect) float64 {
+	return rtreego.Point(p).MinDist((*rtreego.Rect)(r))
+}
