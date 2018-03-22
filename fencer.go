5aed7c0acff0a73c29a17b487185acc328633612
Marcin Bilski <gyamtso@gmail.com>
2018-03-22 14:25:58 +0100

    Make it so go get github.com/bilus/fencer pulls in the whole library.

--- fencer.go
+++ fencer.go
@@ -1,1 +1,9 @@
 package fencer
+
+// Import packages so they are pulled in using `go get github.com/bilus/fencer`
+import (
+	_ "github.com/bilus/fencer/feature"
+	_ "github.com/bilus/fencer/geo"
+	_ "github.com/bilus/fencer/index"
+	_ "github.com/bilus/fencer/query"
+)
