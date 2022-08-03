2ac5064fbff70834d9808be3d2cdb9f3298bd18a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-03 21:29:32 +0800

    Delete fencer.go

--- fencer.go
+++
@@ -1,9 +0,0 @@
-package fencer // import "github.com/bilus/fencer"
-
-// Import packages so they are pulled in using `go get github.com/bilus/fencer`
-import (
-	_ "github.com/bilus/fencer/feature"
-	_ "github.com/bilus/fencer/geo"
-	_ "github.com/bilus/fencer/index"
-	_ "github.com/bilus/fencer/query"
-)
